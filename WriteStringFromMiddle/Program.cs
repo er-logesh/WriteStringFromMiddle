65c4bcf038e0ccdcdcc74cc1efe64e62adba5ff4
er-logesh <47050391+er-logesh@users.noreply.github.com>
2021-03-05 09:17:48 +0530

    Initial upload

---
+++ WriteStringFromMiddle/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace WriteStringFromMiddle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give your input here.");
+            string strInput = Console.ReadLine().Trim();
+            int midIndex = (strInput.Length % 2) == 0 ? (strInput.Length / 2) - 1 : (strInput.Length / 2);
+            string tempString = string.Empty;
+            for (int index = midIndex; index < strInput.Length; index++)
+            {
+                tempString += strInput[index];
+                Console.WriteLine(tempString);
+            }
+            for (int index = 0; index < midIndex; index++)
+            {
+                tempString += strInput[index];
+                Console.WriteLine(tempString);
+            }
+        }
+    }
+}
